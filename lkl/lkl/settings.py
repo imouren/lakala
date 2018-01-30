9e7191edc36ea06cbc079a3d8d0ebe9181c324ed
imouren <mouren11@163.com>
2018-01-30 22:29:55 +0800

    x

--- lkl/lkl/settings.py
+++ lkl/lkl/settings.py
@@ -155,7 +155,7 @@ SUIT_CONFIG = {
         {'app': 'auth', 'icon': 'icon-lock', 'models': ('user', 'group')},
         {'app': 'user', 'icon': 'icon-leaf', 'models': ('UserProfile', 'UserAddress', 'UserPos', 'UserTrade')},
         {'label': u'刷卡数据', 'icon': 'icon-star', 'app': 'user', 'models': ('LKLTrade01', )},
-        {'label': u'拉卡拉', 'icon': 'icon-star', 'url': 'http://mposa.lakala.com/', 'blank': True},
+        {'label': u'拉卡拉', 'icon': 'icon-star', 'url': 'https://mposa.lakala.com/', 'blank': True},
     ),
 
     # misc
