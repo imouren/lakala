11b04bf8215154b34ed1c43911bab18999e0f976
imouren <mouren11@163.com>
2018-03-13 18:26:37 +0800

    x

--- lkl/user/site.py
+++ lkl/user/site.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from django.http import HttpResponse
@@ -6,6 +7,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.contrib.admin.views.decorators import staff_member_required
 from . import site_utils
+from .utils import rclient
 
 
 # @cache_page(3600)
@@ -18,19 +20,28 @@ def income(request):
     4. 已经提现、未提现
     5. 下级分点的提现和未提现
     """
-    pos_data = site_utils.get_boss_pos_info()
-    d1_data = site_utils.get_boss_d1_info()
-    d0_data = site_utils.get_boss_d0_info()
-    urmb, child_urmb = site_utils.get_all_urmb()
-    tx_rmb = site_utils.get_all_txrmb()
-    data = {
-        "d1_data": sorted(d1_data.iteritems()),
-        "d0_data": sorted(d0_data.iteritems()),
-        "pos_data": sorted(pos_data.iteritems()),
-        "urmb": "%.2f" % (urmb / 100.0),
-        "child_urmb": "%.2f" % (child_urmb / 100.0),
-        "tx_rmb": "%.2f" % (tx_rmb / 100.0),
-    }
+    # 缓存
+    key = 'lkl_income_page'
+    data_str = rclient.get(key)
+    if not data_str:
+        pos_data = site_utils.get_boss_pos_info()
+        d1_data = site_utils.get_boss_d1_info()
+        d0_data = site_utils.get_boss_d0_info()
+        urmb, child_urmb = site_utils.get_all_urmb()
+        tx_rmb = site_utils.get_all_txrmb()
+        data = {
+            "d1_data": sorted(d1_data.iteritems()),
+            "d0_data": sorted(d0_data.iteritems()),
+            "pos_data": sorted(pos_data.iteritems()),
+            "urmb": "%.2f" % (urmb / 100.0),
+            "child_urmb": "%.2f" % (child_urmb / 100.0),
+            "tx_rmb": "%.2f" % (tx_rmb / 100.0),
+        }
+        data_str = json.dumps(data)
+        rclient.set(key, data_str)
+        rclient.expire(key, 3600)
+    else:
+        data = json.loads(data_str)
     return render(request, "admin/income.html", data)
 
 
