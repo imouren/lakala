f605ccecad12b2b6937007f2b894640eabb184e1
imouren <mouren11@163.com>
2018-05-21 21:06:08 +0800

    x

--- lkl/lkl/utils.py
+++ lkl/lkl/utils.py
@@ -29,6 +29,17 @@ def get_client_ip(request):
 
 
 def wx_tixian(open_id, fen, name, user_ip):
+    """
+    {'partner_trade_no': '1498390922201805216403871295',
+    'payment_time': '2018-05-21 21:03:15',
+    'nonce_str': 'sMDj3abwf1y5zUhci6xDxDCiIVARFdqT',
+    'return_code': 'SUCCESS',
+    'mch_appid': 'wx3274fa5ad5bb8cbc',
+    'payment_no': '1000018301201805217958360686',
+    'mchid': '1498390922',
+    'return_msg': None,
+    'result_code': 'SUCCESS'}
+    """
     from .wx_pay import WxPay
     pay = WxPay()
     api_cert_path = "/root/wxpay/apiclient_cert.pem"
@@ -42,4 +53,4 @@ def wx_tixian(open_id, fen, name, user_ip):
         "desc": u"分润发放"
     }
     res = pay.enterprise_payment(api_cert_path, api_key_path, **data)
-    print res
+    return res
