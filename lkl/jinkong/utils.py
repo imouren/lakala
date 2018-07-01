b2a392bb4e98e836cccf85d5cd28a8536e498faf
imouren <mouren11@163.com>
2018-07-01 23:07:48 +0800

    x

--- lkl/jinkong/utils.py
+++ lkl/jinkong/utils.py
@@ -1,2 +1,60 @@
 # -*- coding: utf-8 -*-
-from lkl.tsms import send_tsms_multi
+import sys
+import warnings
+import requests
+from bs4 import BeautifulSoup
+from lkl import utils, config
+from jinkong.dbutils import get_token_code, disable_token
+from .prov_code import PROV_CODE
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+warnings.filterwarnings("ignore")
+
+TIMEOUT = 120  # 超时时间
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    'Content-type': 'application/x-www-form-urlencoded',
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) C,hrome/43.0.2357.124 Safari/537.36",
+}
+
+
+def get_cookies(cookie_string):
+    cookies = {}
+    for cookie in cookie_string.split(";"):
+        cookie = cookie.strip()
+        k, v = cookie.split("=", 1)
+        cookies[k] = v
+    return cookies
+
+
+def get_current_prov(user, merchant_code):
+    token = get_token_code()
+    cookies = get_cookies(token)
+    url = "http://119.18.194.36/miniIf/miniModifyPlace?mno=%s" % merchant_code
+    r = requests.get(url, cookies=cookies)
+    html = r.content.decode("utf-8")
+    soup = BeautifulSoup(html)
+    aform = soup.find(id="mercInfoForm")
+    div = aform.find_all("div")[2]
+    prov = div.find_all("label")[1].text
+    return prov
+
+
+def change_prov(user, merchant_code, province):
+    token = get_token_code()
+    cookies = get_cookies(token)
+    data = {
+        "province": province,
+        "mercId": merchant_code
+    }
+    url = "http://119.18.194.36/miniIf/miniSavePlaceData"
+    r = requests.post(url, data=data, cookies=cookies)
+    return r.ok
+
